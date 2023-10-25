32eaedb07c13fadfc099ef47ff96d071aeb4f5c8
Eldar Čelik <eldar@appit.ba>
2023-10-25 21:04:54 +0200

    Typescript fix for container element in createRoot and added Strict Mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
-import './index.css';
-import App from './App';
+import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
 
 const container = document.getElementById('root');
-const root = createRoot(container!); // createRoot(container!) if you use TypeScript
-root.render(<App />);
+const root = createRoot(container as HTMLElement);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
