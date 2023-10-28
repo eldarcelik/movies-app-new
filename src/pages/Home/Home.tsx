0268899216efd063a0a7a5a47c069018459a01db
Eldar Čelik <54398506+eldarcelik@users.noreply.github.com>
2023-10-28 18:44:03 +0200

    Packages updates and starting error fixes (#1)
    * Packages updates and starting error fixes
    
    * Some dependencies moved to devDependencies
    
    * Shorthand for return statement in arrow function
    
    * Typescript fix for container element in createRoot and added Strict Mode
    
    * Added TS path alias for src folder
    
    * Typescript and react-script dependencies moved to devDependencies
    
    ---------
    
    Co-authored-by: Eldar Čelik <eldar@appit.ba>

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react';
-import { MoviesShowsContext } from '../../Context';
-import Navbar from '../../components/Navbar/Navbar';
-import ItemCard from '../../components/Item/ItemCard';
-import Loading from '../../components/Loading/Loading';
-import { CONTENT_TYPE } from '../../constants';
-import { IShow, IMovie } from '../../types';
+import { MoviesShowsContext } from 'Context';
+import Navbar from 'components/Navbar/Navbar';
+import ItemCard from 'components/Item/ItemCard';
+import Loading from 'components/Loading/Loading';
+import { CONTENT_TYPE } from 'constantValues';
+import { IShow, IMovie } from 'types';
 import './Home.css';
 
 export default function Home() {
