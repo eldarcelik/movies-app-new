0156b17bbd67284d8754c723f1b9b5fc5845e90c
Eldar Čelik <eldar.celik21@gmail.com>
2023-11-03 10:30:47 +0100

    chore[7]: Added TS Compiler to Lint-Staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,11 +1,6 @@
-module.exports = {
-  "*.{ts,tsx,css,json}": [
-    "prettier --write"
-  ],
-  "*.{ts,tsx,json}": [
-    "eslint --fix"
-  ],
-  "*.css": [
-    "stylelint --fix"
-  ]
-}
+export default {
+  '*.{ts,tsx,css,json}': ['prettier --write'],
+  '*.{ts,tsx,json}': ['eslint --fix'],
+  '*.css': ['stylelint --fix'],
+  '**/*.ts?(x)': () => 'tsc -p tsconfig.json --noEmit',
+};
