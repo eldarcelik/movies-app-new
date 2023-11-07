969575bb5af4e26869fa93e9da751f87b9c10b2c
Eldar Čelik <eldar.celik21@gmail.com>
2023-11-07 09:10:18 +0100

    Created MovieOrShow test

---
+++ src/pages/MovieOrShow/MovieOrShow.test.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { render, screen } from '@testing-library/react';
+
+import { fetchData } from 'apis/fetchData';
+
+import MovieOrShow from './MovieOrShow';
+
+jest.mock('apis/fetchData');
+
+describe('MovieOrShow component', () => {
+  beforeEach(() => jest.clearAllMocks());
+
+  test('should render movie or show item when api responds', async () => {
+    (fetchData as jest.Mock).mockResolvedValue({
+      results: [{ name: 'spiderman' }],
+    });
+    render(<MovieOrShow />);
+    const gifElement = screen.getByAltText(/loading/i);
+
+    expect(gifElement).toBeInTheDocument();
+
+    // TODO: Check how to implement reading API mock response
+    // await waitFor(() => {
+    //   screen.getByText('spiderman');
+    // });
+  });
+});
