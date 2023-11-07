4b587810099caef7271a962f0511d8bf6cff2a84
Eldar Čelik <eldar.celik21@gmail.com>
2023-11-07 09:09:11 +0100

    chore: [#7] Updated App.test.tsx file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,10 +2,22 @@ import React from 'react';
 
 import { render, screen } from '@testing-library/react';
 
+import { fetchData } from 'apis/fetchData';
+
 import App from './App';
 
-test('renders image alt content', () => {
-  render(<App />);
-  const linkElement = screen.getByAltText(/loading/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('apis/fetchData');
+
+describe('App component', () => {
+  beforeEach(() => jest.clearAllMocks());
+
+  test('should render image alt content', async () => {
+    (fetchData as jest.Mock).mockResolvedValue({
+      results: [{ name: 'spiderman' }],
+    });
+    render(<App />);
+    const gifElement = screen.getByAltText(/loading/i);
+
+    expect(gifElement).toBeInTheDocument();
+  });
 });
