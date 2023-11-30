9e0b6dd72c8c302dc666cb15e6ad788f6db9c3b6
Eldar Čelik <eldar.celik21@gmail.com>
2023-11-30 19:22:16 +0100

    refactor: [#28] Added async/await in APIs functions

--- src/apis/getItem.ts
+++ src/apis/getItem.ts
@@ -2,9 +2,9 @@ import { API_BASE_URL, API_KEY } from '@/constants/constantValues';
 import getData from '@/helpers/getData';
 import { ContentType, IItem } from '@/types/types';
 
-const getItem = (contentType: ContentType, id: string): Promise<IItem> => {
+const getItem = async (contentType: ContentType, id: string): Promise<IItem> => {
   const ITEM_URL = `${API_BASE_URL}${contentType}/${id}?api_key=${API_KEY}&append_to_response=videos`;
-  const item = getData<IItem>(ITEM_URL);
+  const item = await getData<IItem>(ITEM_URL);
 
   return item;
 };
