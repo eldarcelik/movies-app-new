e3c6b6bb462e1eb8a8f92b9ea45dd1d9bb20e0f1
Eldar Čelik <eldar.celik21@gmail.com>
2024-01-08 21:07:58 +0100

    refactor: [#28] Refactored getItems and getItem

--- src/apis/getItem.ts
+++ src/apis/getItem.ts
@@ -2,11 +2,10 @@ import { API_BASE_URL, API_KEY } from '@/constants/constantValues';
 import getData from '@/helpers/getData';
 import { ContentType, IItem } from '@/types/types';
 
-const getItem = async (contentType: ContentType, id: string): Promise<IItem> => {
-  const ITEM_URL = `${API_BASE_URL}${contentType}/${id}?api_key=${API_KEY}&append_to_response=videos`;
-  const item = await getData<IItem>(ITEM_URL);
+const formatUrl = (contentType: ContentType, id: string): string =>
+  `${API_BASE_URL}${contentType}/${id}?api_key=${API_KEY}&append_to_response=videos`;
 
-  return item;
-};
+const getItem = async (contentType: ContentType, id: string): Promise<IItem> =>
+  getData<IItem>(formatUrl(contentType, id));
 
 export default getItem;
