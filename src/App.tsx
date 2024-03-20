a86c293bc300779509358b3cc1ca6aeee1209f01
Eldar Čelik <eldar.celik21@gmail.com>
2024-03-20 22:10:54 +0100

    feat: [#39] Added index entry point for ResetPassword

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { MoviesShowsProvider } from './context';
 import Login from './pages/Account/Login';
 import Registration from './pages/Account/Registration';
 import RequestResetPassword from './pages/Account/RequestResetPassword';
-import ResetPassword from './pages/Account/ResetPassword/ResetPassword';
+import ResetPassword from './pages/Account/ResetPassword';
 import Error from './pages/Error/Error';
 import Home from './pages/Home';
 import MovieOrShow from './pages/MovieOrShow';
